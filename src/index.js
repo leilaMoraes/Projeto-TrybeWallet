c6eed0624f19a87ca919276d11523954ca2e25e1
Leila Moraes <leila.violino@gmail.com>
2022-10-31 16:45:35 -0300

    configuração inicial da store e dos reducers

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -9,7 +10,9 @@ ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
     <BrowserRouter>
-      <App />
+      <Provider store={ store }>
+        <App />
+      </Provider>
     </BrowserRouter>,
   );
 
